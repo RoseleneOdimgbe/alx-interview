49f0655e32d45867cb2fa7fed571deb22e10f971
RoseleneOdimgbe <babymesa98@gmail.com>
2024-10-25 05:51:59 +0100

    corrections

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,7 +4,7 @@
 
 
 def island_perimeter(grid):
-    """Function that returns the perimeter of the island described in the grid"""
+    """Function that returns the perimeter of island described in grid"""
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
